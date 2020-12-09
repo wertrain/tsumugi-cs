d57fc2047b59839817ce355879470a9a0fcf5d13
wertrain <wertrain@gmail.com>
2020-12-09 22:39:18 +0900

    Change projects .NetCore to .NetStandard

--- Tsumugi/Tsumugi/Core.cs
+++ Tsumugi/Tsumugi/Core.cs
@@ -1,9 +1,23 @@
 ﻿using System;
+using System.IO;
 
 namespace Tsumugi
 {
     public class Core
     {
         public static readonly string Version = "0.0.0.0";
+
+        public static void Compile()
+        {
+
+        }
+
+        public static void Execute(string script)
+        {
+            using (var reader = new StringReader(script))
+            {
+                char c = (char)reader.Peek();
+            }
+        }
     }
 }
