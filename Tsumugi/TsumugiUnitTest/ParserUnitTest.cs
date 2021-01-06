74a52560b445cbe37984917aa13c9ea035a0043d
wertrain <wertrain@gmail.com>
2021-01-07 00:10:21 +0900

    テストのリファクタ

--- Tsumugi/TsumugiUnitTest/ParserUnitTest.cs
+++ Tsumugi/TsumugiUnitTest/ParserUnitTest.cs
@@ -13,8 +13,11 @@ namespace TsumugiUnitTest
     [TestClass]
     public class ParserUnitTest
     {
+        /// <summary>
+        /// Let 文のテスト
+        /// </summary>
         [TestMethod]
-        public void TestMethodParserBasic()
+        public void TestMethodParserLetStatement()
         {
             var script = "" +
                         "let x = 5;" +
@@ -41,8 +44,11 @@ namespace TsumugiUnitTest
             }
         }
 
+        /// <summary>
+        /// 正しくない Let 文でのテスト
+        /// </summary>
         [TestMethod]
-        public void TestMethodParserBasicError()
+        public void TestMethodParserLetStatementError()
         {
             var script = "" +
                         "let x 5;" +
@@ -54,9 +60,14 @@ namespace TsumugiUnitTest
             var parser = new Parser(lexer);
             var root = parser.ParseProgram();
 
-            Assert.IsTrue(parser.Logger.Count(Logger.Categories.Error) == 3, parser.Logger.GetHistory(Logger.Categories.Error));
+            Assert.IsTrue(parser.Logger.Count(Logger.Categories.Error) >= 3, parser.Logger.GetHistory(Logger.Categories.Error));
         }
 
+        /// <summary>
+        /// Let 文のテスト
+        /// </summary>
+        /// <param name="statement"></param>
+        /// <param name="name"></param>
         private void letStatement(IStatement statement, string name)
         {
             Assert.AreEqual(
@@ -72,16 +83,19 @@ namespace TsumugiUnitTest
 
             Assert.AreEqual(
                 letStatement.Name.Value, name,
-                $"識別子が間違っています。"
+                "識別子が間違っています。"
             );
 
             Assert.AreEqual(
                 letStatement.Name.TokenLiteral(), name,
-                $"識別子のリテラルが間違っています。"
+                "識別子のリテラルが間違っています。"
             );
 
         }
 
+        /// <summary>
+        /// Return 文のテスト
+        /// </summary>
         [TestMethod]
         public void TestMethodParserReturnStatement()
         {
@@ -115,12 +129,9 @@ namespace TsumugiUnitTest
             }
         }
 
-        private void checkParserErrors(Parser parser)
-        {
-            if (parser.Logger.Count(Logger.Categories.Error) == 0) return;
-            Assert.Fail(parser.Logger.GetHistory(Logger.Categories.Error));
-        }
-
+        /// <summary>
+        /// ToCode によるコード復元のテスト
+        /// </summary>
         [TestMethod]
         public void TestMethodParserToCode()
         {
@@ -147,6 +158,9 @@ namespace TsumugiUnitTest
             Assert.AreEqual(code, root.ToCode(), "Root.ToCode() の結果が間違っています。");
         }
 
+        /// <summary>
+        /// 識別子式のテスト
+        /// </summary>
         [TestMethod]
         public void TestMethodParserIdentifierExpression()
         {
@@ -173,6 +187,7 @@ namespace TsumugiUnitTest
             {
                 Assert.Fail("Expression が Identifier ではありません。");
             }
+
             if (ident.Value != "foobar")
             {
                 Assert.Fail("ident.Value が foobar ではありません。");
@@ -183,6 +198,9 @@ namespace TsumugiUnitTest
             }
         }
 
+        /// <summary>
+        /// 整数値式のテスト
+        /// </summary>
         [TestMethod]
         public void TestMethodParserIntegerExpression()
         {
@@ -211,12 +229,197 @@ namespace TsumugiUnitTest
             }
             if (integerLiteral.Value != 12345)
             {
-                Assert.Fail("ident.Value が 12345 ではありません。");
+                Assert.Fail("integerLiteral.Value が 12345 ではありません。");
             }
             if (integerLiteral.TokenLiteral() != "12345")
             {
-                Assert.Fail("ident.TokenLiteral が 12345 ではありません。");
+                Assert.Fail("integerLiteral.TokenLiteral が 12345 ではありません。");
             }
+        }
+
+        /// <summary>
+        /// 前置演算子式のテスト
+        /// </summary>
+        [TestMethod]
+        public void TestMethodParsertPrefixExpression()
+        {
+            var tests = new[] {
+                ("!5", "!", 5),
+                ("-15", "-", 15),
+            };
+
+            foreach (var (input, op, value) in tests)
+            {
+                var lexer = new Lexer(input);
+                var parser = new Parser(lexer);
+                var root = parser.ParseProgram();
+                checkParserErrors(parser);
+
+                Assert.AreEqual(
+                    root.Statements.Count, 1,
+                    "Root.Statementsの数が間違っています。"
+                );
+
+                var statement = root.Statements[0] as ExpressionStatement;
+                if (statement == null)
+                {
+                    Assert.Fail("statement が ExpressionStatement ではありません。");
+                }
+
+                var expression = statement.Expression as PrefixExpression;
+                if (expression == null)
+                {
+                    Assert.Fail("expression が PrefixExpression ではありません。");
+                }
+
+                if (expression.Operator != op)
+                {
+                    Assert.Fail($"Operator が {expression.Operator} ではありません。({op})");
+                }
+
+                testIntegerLiteral(expression.Right, value);
+            }
+        }
+
+        /// <summary>
+        /// 中置演算子式のテスト
+        /// </summary>
+        [TestMethod]
+        public void TestMethodParsertInfixExpression()
+        {
+            var tests = new[] {
+                ("1 + 1;", 1, "+", 1),
+                ("1 - 1;", 1, "-", 1),
+                ("1 * 1;", 1, "*", 1),
+                ("1 / 1;", 1, "/", 1),
+                ("1 < 1;", 1, "<", 1),
+                ("1 > 1;", 1, ">", 1),
+                ("1 == 1;", 1, "==", 1),
+                ("1 != 1;", 1, "!=", 1),
+            };
+
+            foreach (var (input, leftValue, op, rightValue) in tests)
+            {
+                var lexer = new Lexer(input);
+                var parser = new Parser(lexer);
+                var root = parser.ParseProgram();
+                checkParserErrors(parser);
+
+                Assert.AreEqual(
+                    root.Statements.Count, 1,
+                    "Root.Statementsの数が間違っています。"
+                );
+
+                var statement = root.Statements[0] as ExpressionStatement;
+                if (statement == null)
+                {
+                    Assert.Fail("statement が ExpressionStatement ではありません。");
+                }
+                
+                testInfixExpression(statement.Expression, leftValue, op, rightValue);
+            }
+        }
+
+        /// <summary>
+        /// パースでエラーが発生すれば Fail とする
+        /// </summary>
+        /// <param name="parser">チェックするパーサー</param>
+        private void checkParserErrors(Parser parser)
+        {
+            if (parser.Logger.Count(Logger.Categories.Error) == 0) return;
+            Assert.Fail(parser.Logger.GetHistory(Logger.Categories.Error));
+        }
+
+        /// <summary>
+        /// リテラルごとの式テスト
+        /// </summary>
+        /// <param name="expression"></param>
+        /// <param name="expected"></param>
+        private void testLiteralExpression(IExpression expression, object expected)
+        {
+            switch (expected)
+            {
+                case int intValue:
+                    testIntegerLiteral(expression, intValue);
+                    break;
+                case string stringValue:
+                    testIdentifier(expression, stringValue);
+                    break;
+                default:
+                    Assert.Fail("予期せぬ型です。");
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// 識別子式のテスト
+        /// </summary>
+        /// <param name="expression">チェックする式</param>
+        /// <param name="value">式の値</param>
+        private void testIdentifier(IExpression expression, string value)
+        {
+            var identifier = expression as Identifier;
+
+            if (identifier == null)
+            {
+                Assert.Fail("Expression が Identifier ではありません。");
+            }
+            if (identifier.Value != value)
+            {
+                Assert.Fail($"identifier.Value が {value} ではありません。({identifier.Value})");
+            }
+            if (identifier.TokenLiteral() != value)
+            {
+                Assert.Fail($"identifier.TokenLiteral が {value} ではありません。({identifier.TokenLiteral()})");
+            }
+        }
+
+        /// <summary>
+        /// 整数値式のテスト
+        /// </summary>
+        /// <param name="expression"></param>
+        /// <param name="value"></param>
+        public void testIntegerLiteral(IExpression expression, int value)
+        {
+            var integerLiteral = expression as IntegerLiteral;
+
+            if (integerLiteral == null)
+            {
+                Assert.Fail("Expression が IntegerLiteral ではありません。");
+            }
+            if (integerLiteral.Value != value)
+            {
+                Assert.Fail($"integerLiteral.Value が {value} ではありません。");
+            }
+            if (integerLiteral.TokenLiteral() != $"{value}")
+            {
+                Assert.Fail($"ident.TokenLiteral が {value} ではありません。");
+            }
+        }
+
+        /// <summary>
+        /// 中置演算子式のテスト
+        /// </summary>
+        /// <param name="expression">式</param>
+        /// <param name="left">左辺</param>
+        /// <param name="op">演算子</param>
+        /// <param name="right">右辺</param>
+        private void testInfixExpression(IExpression expression, object left, string op, object right)
+        {
+            var infixExpression = expression as InfixExpression;
+            if (infixExpression == null)
+            {
+                Assert.Fail("expression が InfixExpression ではありません。");
+            }
+
+            testLiteralExpression(infixExpression.Left, left);
+
+            if (infixExpression.Operator != op)
+            {
+                Assert.Fail($"Operator が {infixExpression.Operator} ではありません。({op})");
+            }
+
+            testLiteralExpression(infixExpression.Right, right);
         }
     }
 }
