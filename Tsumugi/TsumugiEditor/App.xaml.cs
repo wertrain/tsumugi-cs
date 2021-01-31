6a8746c4532091d67d6d5f394e00dcda06e3966e
wertrain <wertrain@gmail.com>
2021-01-31 16:23:09 +0900

    Tsumugi エディタを追加

---
+++ Tsumugi/TsumugiEditor/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TsumugiEditor
+{
+    /// <summary>
+    /// App.xaml の相互作用ロジック
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            System.Threading.Thread.CurrentThread.CurrentUICulture = System.Globalization.CultureInfo.CurrentCulture;
+            Localize.Localizer.SetStringLocalizer(new Localize.EmbeddedResourceStringLocalizer());
+        }
+    }
+}
