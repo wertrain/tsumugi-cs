c8d745b301c0d2123f2389cd682cbf5098cdf814
wertrain <wertrain@gmail.com>
2021-01-31 18:43:54 +0900

    Tsumugi のシンタックスハイライトに対応

--- Tsumugi/TsumugiEditor/MainWindow.xaml.cs
+++ Tsumugi/TsumugiEditor/MainWindow.xaml.cs
@@ -20,9 +20,39 @@ namespace TsumugiEditor
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
+
+            using (var stream = System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream("TsumugiEditor.Resources.Tsumugi.xshd"))
+            {
+                using (var reader = new System.Xml.XmlTextReader(stream))
+                {
+                    _textEditor.SyntaxHighlighting = ICSharpCode.AvalonEdit.Highlighting.Xshd.HighlightingLoader.Load(
+                        reader, ICSharpCode.AvalonEdit.Highlighting.HighlightingManager.Instance);
+                }
+            }
+
+            _textEditor.Options.ShowSpaces = true;
+            _textEditor.Options.ShowTabs = true;
+        }
+
+        /// <summary>
+        /// メニューアイテム「テーマ」の子アイテムがクリックされた時のイベント
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MenuItemTheme_Click(object sender, RoutedEventArgs e)
+        {
+            MenuItem menuItem = sender as MenuItem;
+            foreach (MenuItem sibling in (menuItem.Parent as MenuItem).Items)
+            {
+                sibling.IsChecked = menuItem == sibling;
+            }
+            _dockingManager.Theme = menuItem.Tag as Xceed.Wpf.AvalonDock.Themes.Theme;
         }
     }
 }
